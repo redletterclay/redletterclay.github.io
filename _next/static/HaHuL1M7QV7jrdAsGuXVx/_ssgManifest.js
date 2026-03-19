3776573d37ab7b12a5fb89247c0e39307ce11ba1
david-ball <david-ball@users.noreply.github.com>
2026-03-19 19:29:43 +0000

    deploy: 1e2027793134d447d6e2c5304d28442bb05f8378

---
+++ _next/static/HaHuL1M7QV7jrdAsGuXVx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fjournal\u002F[category]","\u002Fjournal\u002F[category]\u002F[slug]","\u002Fjournal\u002Fpage\u002F[pageNumber]","\u002Fproducts\u002F[sku]","\u002Fshop\u002F[category]","\u002Fshop\u002F[category]\u002Fp\u002F[pageNumber]","\u002Fshop\u002Fp\u002F[pageNumber]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- justfile
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env just --justfile
-
-# hello is recipe's name
-build:
-  bundle exec jekyll build
-
-serve:
-  bundle exec jekyll serve
