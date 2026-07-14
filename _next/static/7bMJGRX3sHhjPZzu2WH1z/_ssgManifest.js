4c0fd44bf772784aff60a277846c068d9658d7c8
david-ball <david-ball@users.noreply.github.com>
2026-07-14 21:47:21 +0000

    deploy: 3386e5ddc5c86a3b108ad7b464e3b2e2aecd2350

--- _next/static/7bMJGRX3sHhjPZzu2WH1z/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fjournal\u002F[category]","\u002Fjournal\u002F[category]\u002F[slug]","\u002Fjournal\u002Fpage\u002F[pageNumber]","\u002Fproducts\u002F[sku]","\u002Fshop\u002F[category]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
